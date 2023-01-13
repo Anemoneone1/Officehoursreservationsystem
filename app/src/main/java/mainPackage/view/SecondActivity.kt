2d35b930f5b238bab07ad621e171c88107ac6c3c
MPokotilov <mpokotilov2@gmail.com>
2023-01-13 22:02:50 +0100

    git commit max_1.4

--- app/src/main/java/mainPackage/view/SecondActivity.kt
+++ app/src/main/java/mainPackage/view/SecondActivity.kt
@@ -1,12 +1,20 @@
 package mainPackage.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import com.example.officehoursreservationsystem.R
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val secondButAct = findViewById<ImageButton>(R.id.image_button_2)
+        secondButAct.setOnClickListener{
+            val Intent = Intent(this, Main2Activity::class.java)
+            startActivity(Intent)
+        }
     }
 }
--- app/src/main/java/mainPackage/view/StudentActivity.kt
+++ app/src/main/java/mainPackage/view/StudentActivity.kt
@@ -1,12 +1,20 @@
 package mainPackage.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import com.example.officehoursreservationsystem.R
 
 class StudentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student)
+
+        val butAct = findViewById<ImageButton>(R.id.image_button_3)
+        butAct.setOnClickListener {
+            val Intent = Intent(this, SecondActivity::class.java)
+            startActivity(Intent)
+        }
     }
 }
