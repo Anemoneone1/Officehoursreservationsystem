37485b2d93cceadd29093148841e9f127d2572d4
MPokotilov <mpokotilov2@gmail.com>
2023-01-15 20:14:26 +0100

    git commit max_2.4

--- app/src/main/java/mainPackage/view/TeacherCreationActivity.kt
+++ app/src/main/java/mainPackage/view/TeacherCreationActivity.kt
@@ -2,10 +2,11 @@ package mainPackage.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.officehoursreservationsystem.R
 
 class TeacherCreationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_teacher_creation_acrivity)
+        setContentView(R.layout.activity_teacher_creation_activity)
     }
 }
