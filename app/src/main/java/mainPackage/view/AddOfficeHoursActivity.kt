8499dd5aec67e64b5e4ae8263352f123ce978788
MPokotilov <mpokotilov2@gmail.com>
2023-01-15 22:38:33 +0100

    git commit max_2.5

---
+++ app/src/main/java/mainPackage/view/AddOfficeHoursActivity.kt
@@ -0,0 +1,11 @@
+package mainPackage.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddOfficeHoursActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_office_hours)
+    }
+}
