a6e77c2cb87f084de7d92f6f7ec5eeafe5bf2153
MPokotilov <mpokotilov2@gmail.com>
2023-01-15 20:13:49 +0100

    git commit max_2.3

---
+++ app/src/main/java/mainPackage/view/TeacherCreationActivity.kt
@@ -0,0 +1,11 @@
+package mainPackage.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TeacherCreationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_teacher_creation_acrivity)
+    }
+}
